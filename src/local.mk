6279617d68384992d88ab0e63d191e517b3a0d79
mlvzk <mlvzk@protonmail.com>
2021-04-06 21:47:46 +0200

    build: add jansson

--- src/local.mk
+++ src/local.mk
@@ -91,7 +91,7 @@ remove_ldadd =
 # must precede $(LIBINTL) in order to ensure we use GNU getopt.
 # But libcoreutils.a must also follow $(LIBINTL), since libintl uses
 # replacement functions defined in libcoreutils.a.
-LDADD = src/libver.a lib/libcoreutils.a $(LIBINTL) lib/libcoreutils.a
+LDADD = src/libver.a lib/libcoreutils.a $(LIBINTL) lib/libcoreutils.a -ljansson
 
 # First, list all programs, to make listing per-program libraries easier.
 # See [ below.
